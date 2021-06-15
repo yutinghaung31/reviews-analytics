5913d41eb7b1bfcf052c3ec421a8b42f6e47d755
Yu-Ting Huang <yutinghuang31@gmail.com>
2021-06-15 11:01:10 -0700

    calc

--- read.py
+++ read.py
@@ -6,8 +6,10 @@ with open('reviews.txt', 'r') as f :
         count += 1 # count = count + 1
         if count % 1000 == 0:
             print(len(date))
-print(len(date))
+print('檔案讀取完了,總共有', len(date), '筆資料')
 
-print(date[0])
-print('---------------')
-print(date[1])
+sum_len = 0
+for d in date:
+    sum_len = sum_len + len(d)
+
+print('留言的平均長度為', sum_len/len(date))
