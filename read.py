61a48c50a096da42e85ea3276a960c08714cc4fe
Yu-Ting Huang <yutinghuang31@gmail.com>
2021-07-13 18:06:15 -0700

    add word count function

--- read.py
+++ read.py
@@ -1,23 +1,61 @@
-date = []
+data = []
 count = 0
 with open('reviews.txt', 'r') as f :
     for line in f :
-        date.append(line)
+        data.append(line)
         count += 1 # count = count + 1
         if count % 1000 == 0:
-            print(len(date))
-print('檔案讀取完了,總共有', len(date), '筆資料')
+            print(len(data))
+print('檔案讀取完了,總共有', len(data), '筆資料')
 
 sum_len = 0
 for d in date:
     sum_len = sum_len + len(d)
-print('留言的平均長度為', sum_len/len(date))
+print('留言的平均長度為', sum_len/len(data))
 
 
 new = []
-for d in date:
+for d in data:
     if len(d) < 100:
         new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good),'筆留言提到good' )
+
+
+#文字計數
+wc = {} # word_count
+for d in data:
+    words = d.split()
+    for word  in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1 #新增新的key進wc字典
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])    
+
+while True:
+    word = input('請問你想查什麼字：')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為：', wc[word])
+    else:
+        print('這個字沒有出現過喔！')
+
+print('感謝使用本查詢功能')
+
+
